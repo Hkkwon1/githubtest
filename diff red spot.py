a302a05a4d86e033d45a5da7024531409f4e2093
Hkkwon1 <49094007+Hkkwon1@users.noreply.github.com>
2019-07-11 20:07:01 +0900

    Add files via upload

---
+++ diff red spot.py
@@ -0,0 +1,19 @@
+import numpy as np, cv2
+
+img1 =cv2.imread('img/gg1.jpg')
+img2 =cv2.imread('img/gg2.jpg')
+img1_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+img2_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
+diff = cv2.absdiff(img1_gray, img2_gray)
+diff_red = cv2.cvtColor(diff, cv2.COLOR_GRAY2BGR)
+diff_red[:,:,2] = 0
+
+spot = cv2.bitwise_xor(img2, diff_red)
+
+cv2.imshow('img1', img1)
+cv2.imshow('img2', img2)
+cv2.imshow('diff', diff)
+cv2.imshow('spot', spot)
+cv2.waitKey()
+cv2.destroyAllWidows()
